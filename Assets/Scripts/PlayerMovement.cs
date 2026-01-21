8ed58ecdcc415b1429358534621f595a431c6b4a
monsterhands <beinean.evely@gmail.com>
2026-01-21 18:22:24 -0500

    Temp disabled the idle neutral for testing
    commented out in code and unhooked in animator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,13 +24,20 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         animator.SetBool("isWalking", true);
+        //animator.SetBool("isIdle", false);
 
         if(context.canceled)
         {
             animator.SetBool("isWalking", false);
             animator.SetFloat("LastInputX", moveInput.x);
             animator.SetFloat("LastInputY", moveInput.y);
+            //animator.SetBool("isIdle", true);
         }
+
+        //if (animator.GetBool("isIdle"))
+        //{
+        //    animator.SetBool("isIdle", true);
+        //}
 
         moveInput = context.ReadValue<Vector2>();
         animator.SetFloat("InputX", moveInput.x);
